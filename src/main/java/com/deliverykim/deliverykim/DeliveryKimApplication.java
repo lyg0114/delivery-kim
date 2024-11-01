ab77decefcbabe1906d9ffbbbd775946a320db83
lyg0114 <whdnseowkd@naver.com>
2024-11-01 19:58:24 +0900

    init project

---
+++ src/main/java/com/deliverykim/deliverykim/DeliveryKimApplication.java
@@ -0,0 +1,13 @@
+package com.deliverykim.deliverykim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryKimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryKimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deliverykim/deliverykim/DeliveryKimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliverykim.deliverykim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryKimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
